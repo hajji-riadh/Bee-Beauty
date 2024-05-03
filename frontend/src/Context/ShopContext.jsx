ae2c74c4a0c9aeda1a13457fe9ba308f8285824b
Riadh Hajji <hajjiriadh378@gmail.com>
2024-05-03 01:21:55 +0100

    augmenter le nombre d'articles

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -4,7 +4,7 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
-  for (let index = 0; index < 300 + 1; index++) {
+  for (let index = 0; index < 999 + 1; index++) {
     cart[index] = 0;
   }
   return cart;
