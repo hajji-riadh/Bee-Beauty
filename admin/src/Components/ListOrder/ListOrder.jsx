f238e80cc7a0827adb93e4afe2432e023f138058
Riadh Hajji <hajjiriadh378@gmail.com>
2024-05-03 03:52:18 +0100

    liste des commandes

--- admin/src/Components/ListOrder/ListOrder.jsx
+++ admin/src/Components/ListOrder/ListOrder.jsx
@@ -1,55 +1,103 @@
-import { useState } from "react";
 import "./ListOrder.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ListOrder = () => {
-  const [allOrder, setAllOrder] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [all_product, setAll_Product] = useState([]);
 
-  const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allorders")
-      .then((res) => res.json())
-      .then((data) => {
-        setAllOrder(data);
-      });
-  };
+  useEffect(() => {
+    fetch("http://localhost:4000/allproducts")
+      .then((response) => response.json())
+      .then((data) => setAll_Product(data));
+  }, []);
 
-    useEffect(() => {
-      fetchInfo();
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:4000/allusers", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "admin-token": `${localStorage.getItem("auth-token")}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
 
   return (
-    <div className="listorder">
-      <h3>Listes des commandes</h3>
-      <div className="listorder-container">
-        <div className="listorder-item">
-          <p>Nom du client</p>
-          <p>Nom du produit</p>
-          <p>Prix</p>
-          <p>Quantité</p>
-        </div>
+    <div className="list-orders">
+      <h1>{`Liste des commandes`}</h1>
+      <div className="listorders-format-main">
+        <p>Nom de client</p>
+        <p>Nom de produit</p>
+        <p>Image</p>
+        <p>Quantité</p>
+        <p>Prix total</p>
+        <p>Téléphone</p>
+        <p>Confirmer</p>
+        <p>Annuler</p>
       </div>
-      <div className="listorder-allorder">
-        {allOrder.map((order, index) => {
+      <div className="listorders-allorders">
+        <hr />
+        {users.map((user) => {
+          const totalProducts = Object.values(user.cartData).reduce(
+            (a, b) => a + b,
+            0
+          );
           return (
-            <div key={index} className="listorder-item">
-              <p>{order.name}</p>
-              <p>{order.product}</p>
-              <p>{order.price}</p>
-              <p>{order.quantity}</p>
-              <div className="removeuser">
-                <button
-                  className="btn-removeuser"
-                  onClick={() => remove_order(order.id)}
-                >
-                  Retirer
-                </button>
+            totalProducts > 0 && (
+              <div
+                key={user.id}
+                className="listorders-format-main listorders-format"
+              >
+                {Object.entries(user.cartData).map(([productId, quantity]) => {
+                  const product = all_product.find(
+                    (product) => product.id == productId
+                  );
+                  return (
+                    product &&
+                    quantity > 0 && (
+                      <div
+                        key={productId}
+                        className="listorders-format-main listorders-format"
+                      >
+                        <p>{user.name}</p>
+                        <p>{`${product.name}`}</p>
+                        <img
+                          src={product.image}
+                          alt={product.name}
+                          className="listorders-product-icon"
+                        />
+                        <p>{`${quantity}`}</p>
+                        <p>{`${product.new_price * quantity} TND`}</p>
+                        <p>{user.phone}</p>
+                        <button
+                          // ononClick={() => {
+                          //   confirm_order(product.id);
+                          // }}
+                          className="listorders-confirm-btn"
+                        >
+                          Confirmer
+                        </button>
+                        <button
+                          // ononClick={() => {
+                          //   confirm_order(product.id);
+                          // }}
+                          className="listorerds-cancel-btn"
+                        >
+                          Annuler
+                        </button>
+                      </div>
+                    )
+                  );
+                })}
+                <hr />
               </div>
-            </div>
+            )
           );
         })}
       </div>
     </div>
   );
 };
-
 export default ListOrder;
